bacdde2db23ee0f530d7c7bf256c27e63542c6e6
UchuNoKen <vanguardbosses@gmail.com>
2018-08-27 19:02:46 -0400

    IMported Link and NavLink from react-router-dom

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import Header from '../components/Header';
-import ExpenseDashboardPage from '../components/ExpenseDashboadPage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
 
-
-
-
-
-
-
-
-
-const HelpPage = () => (
-    <div>
-      This is from help component.
-    </div>
-);
 
 const AppRouter = () => (
     <BrowserRouter>
