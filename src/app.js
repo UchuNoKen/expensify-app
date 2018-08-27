5c484ad68da0556898c2c4891cdc2d04b53a8ae7
UchuNoKen <vanguardbosses@gmail.com>
2018-08-27 18:23:19 -0400

    Included exact: true in root route

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ const NotFoundPage = () => (
 const Header = () => (
     <header>
       <h1>Expensify</h1>
-      <NavLink to="/" activeClassName="is-active">Dashboard</NavLink>
+      <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
       <NavLink to="/create" activeClassName="is-active"> Create Expense </NavLink>
       <NavLink to="/edit" activeClassName="is-active"> Edit Expense</NavLink>
       <NavLink to="/help" activeClassName="is-active"> Help </NavLink>
