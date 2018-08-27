73829392f4e3b9d7eca0117b5c9313adb0930d80
UchuNoKen <vanguardbosses@gmail.com>
2018-08-27 19:20:53 -0400

    refactored EditExpensePage to log props to the console.

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const EditExpensePage = () => (
+const EditExpensePage = (props) => {
+  console.log(props); 
+  return (
     <div>
       This is from edit expense component.
     </div>
-);
+  );
+}
+
 
 export default EditExpensePage;
