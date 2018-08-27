b29b37743afd93b64eb70f272b0ebfaeb958fe69
UchuNoKen <vanguardbosses@gmail.com>
2018-08-27 15:35:13 -0400

    Setup react-router-dom, created page components

--- src/app.js
+++ src/app.js
@@ -1,5 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+    <div>
+      This is from dashboard component.
+    </div>
+);
+
+
+const AddExpensePage = () => (
+    <div>
+      This is from add expense component.
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+      This is from edit expense component.
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+      This is from help component.
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+      <div>
+        <Route path='/' component={ExpenseDashboardPage} exact={true}/>
+        <Route path='/create' component={AddExpensePage}/>
+        <Route path='/edit' component={EditExpensePage}/>
+        <Route path='/help' component={HelpPage}/>
+      </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
 
